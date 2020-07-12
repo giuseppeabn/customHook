c68573f58eba7fbf7b2062acc0fd07f0e8a86ac0
Giusepppe <giuseppe.varas@bovinoapp.com>
2020-07-11 21:38:28 -0400

    primer commit

---
+++ useFetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect, useRef } from 'react';
+
+//https://www.breakingbadapi.com/api/quotes/1
+
+export const useFetch = url => {
+  const [state, setState] = useState({ data: null, loading: true, erro: null });
+
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+  useEffect(() => {
+    setState({
+      error: null,
+      loading: true,
+      data: null
+    });
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        // console.log('data', data);
+        if (isMounted.current) {
+          setState({
+            error: null,
+            loading: false,
+            data
+          });
+        }
+      });
+  }, [url]);
+  return state;
+};
